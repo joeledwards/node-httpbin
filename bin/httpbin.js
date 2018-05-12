934556cb79756f70abbb321b20acbd5314538664
Joel Edwards <joeledwards@gmail.com>
2018-05-12 00:07:30 -0600

    feat: increased logging detail

--- bin/httpbin.js
+++ bin/httpbin.js
@@ -1,2 +1,17 @@
 #!/usr/bin/env node
-require('../lib/server')({})
+require('../lib/server')(
+  require('yargs')
+  .option('bind-port', {
+    type: 'number',
+    desc: 'TCP port on which the server should listen',
+    default: 1337,
+    alias: ['p']
+  })
+  .option('verbose', {
+    type: 'boolean',
+    desc: 'log detailed info for each request',
+    default: false,
+    alias: ['v']
+  })
+  .argv
+)
